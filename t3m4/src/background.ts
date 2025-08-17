04ca76b4233ba9c7df58ede2b13266f01387586e
babakes <babak.eskandarnezhad@gmail.com>
2025-08-17 12:39:46 -0700

    Update background.ts

--- t3m4/src/background.ts
+++ t3m4/src/background.ts
@@ -30,6 +30,8 @@ interface Message {
 browser.runtime.onMessage.addListener((message: Message) => {
     if (message.type === 'GAME_STATE') {
         console.log("Received game state:", message.data);
-        // sendToC3PO(message.data); // we’ll implement this next
+        if (ws && ws.readyState === WebSocket.OPEN) {
+            ws.send(JSON.stringify(message.data));
+        }
     }
 });
